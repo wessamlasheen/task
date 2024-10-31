da9bbfc09638430147df538bbe341d383b0b4ffd
wessamlasheen <wessamadelgawad@gmail.com>
2024-10-31 14:30:16 +0300

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/task/",
+  base: "/task/", // Match exactly with your repo name
+  build: {
+    outDir: "dist",
+  },
 });
